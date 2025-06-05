0970319b0fc83ea450917202f611814d76ded119
Peesari Sathvik Reddy <reddysathvik2005@gmail.com>
2025-06-05 18:16:50 +0530

    Add files via upload

---
+++ 1_band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city=input("What's the name of the city your grew up in? \n")
+pet_name=input("What's your pet name? \n")
+print("Your Band name could be "+city+" "+pet_name)
